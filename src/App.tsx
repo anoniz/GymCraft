9d46344dbf4aeaea37a6f523b0eacef8c906aee7
anoniz <abdullatifnizamani517@gmail.com>
2024-09-11 01:00:58 +0500

    project started

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Gym Craft is coming soon.
+        </p>
+       <p>Contact: abdullatifnizamani517@gmail.com</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
